9a011d6eedadbc2b185d7ff17cca3d801605bbac
Andrea Vandelli <andrea.vandelli94@gmail.com>
2017-03-16 21:31:14 +0100

    definitions addeed

--- HMM/backward.py
+++ HMM/backward.py
@@ -1,4 +1,5 @@
 def backward(a,e,seq,states):
+	'''implementation of the backward algorithm'''
 	F=[[0.0 for i in range(len(seq)+2)] for s in states]
 	F[-1][-1]=1
 	for s in range(len(states)):
@@ -7,25 +8,18 @@ def backward(a,e,seq,states):
 		for i in range(len(states)-2,0,-1):
 			for w in range(len(states)):
 				F[i][j]=F[i][j]+F[w][j+1]*a.get((states[i],states[w]),0)*e.get((states[w],seq[j]),0)
-			
 	for w in range(len(states)):
 		F[0][0]=F[0][0]+F[w][1]*a.get((states[0],states[w]),0)*e.get((states[w],seq[0]),0)
-	print F
+	return F
 
 
+#input examples
 
+states=['B','Y','N','E'] #list of states
 
+a={('B','Y'):0.2, ('B','N'):0.8, ('Y','Y'):0.7, ('Y','N'):0.2, ('N','Y'):0.1, ('N','N'):0.8, ('Y','E'):0.1, ('N','E'):0.1} #dictionary of transitions probabilities
 
+e={('Y','A'):0.1, ('Y','G'):0.4, ('Y','C'):0.4, ('Y','T'):0.1, ('N','A'):0.25, ('N','G'):0.25, ('N','C'):0.25, ('N','T'):0.25} #dictionary of emission probabilities
 
+seq='ATGCG' #example of sequence
 
-
-
-
-
-states=['s1','s2','s3','s4']
-a={('s1','s2'):0.2, ('s1','s3'):0.8, ('s2','s2'):0.7, ('s2','s3'):0.2, ('s3','s2'):0.1, ('s3','s3'):0.8, ('s2','s4'):0.1, ('s3','s4'):0.1}
-e={('s2','A'):0.1, ('s2','G'):0.4, ('s2','C'):0.4, ('s2','T'):0.1, ('s3','A'):0.25, ('s3','G'):0.25, ('s3','C'):0.25, ('s3','T'):0.25}
-
-seq='ATGCG'
-
-backward(a,e,seq,states)
--- HMM/forward.py
+++ HMM/forward.py
@@ -1,4 +1,5 @@
 def forward(a,e,seq,states):
+	'''Implementation of the forward algorithm'''
 	F=[[0.0 for i in range(len(seq)+2)] for s in states]
 	F[0][0]=1
 	for j in range(1,len(seq)+1):
@@ -8,17 +9,15 @@ def forward(a,e,seq,states):
 			F[i][j]=F[i][j]*e.get((states[i],seq[j-1]),0)
 	for w in range(len(states)):
 		F[-1][-1]=F[-1][-1]+F[w][-2]*a.get((states[w],states[-1]),0)
-	print F
+	return F
 
 
+#input examples
 
+states=['B','Y','N','E'] #list of states
 
+a={('B','Y'):0.2, ('B','N'):0.8, ('Y','Y'):0.7, ('Y','N'):0.2, ('N','Y'):0.1, ('N','N'):0.8, ('Y','E'):0.1, ('N','E'):0.1} #dictionary of transitions probabilities
 
+e={('Y','A'):0.1, ('Y','G'):0.4, ('Y','C'):0.4, ('Y','T'):0.1, ('N','A'):0.25, ('N','G'):0.25, ('N','C'):0.25, ('N','T'):0.25} #dictionary of emission probabilities
 
-states=['s1','s2','s3','s4']
-a={('s1','s2'):0.2, ('s1','s3'):0.8, ('s2','s2'):0.7, ('s2','s3'):0.2, ('s3','s2'):0.1, ('s3','s3'):0.8, ('s2','s4'):0.1, ('s3','s4'):0.1}
-e={('s2','A'):0.1, ('s2','G'):0.4, ('s2','C'):0.4, ('s2','T'):0.1, ('s3','A'):0.25, ('s3','G'):0.25, ('s3','C'):0.25, ('s3','T'):0.25}
-
-seq='ATGCG'
-
-forward(a,e,seq,states)
+seq='ATGCG' #example of sequence
