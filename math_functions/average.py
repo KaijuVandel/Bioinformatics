058baa20effe3754d83165d3c702ce83c492ee6b
Andrea Vandelli <andrea.vandelli94@gmail.com>
2017-03-17 09:39:57 +0100

    average.py updated

--- math_functions/average.py
+++ math_functions/average.py
@@ -1,7 +1,19 @@
+#!/usr/bin/env python   
+import sys
+
 def average(x):
 	'''This program takes in input a list of numbers and returns the average'''
 	value=0.0
 	for i in x:
-		value=value+i
+		value=value+float(i)
 	value=float(value)/len(x)
-	return value		
+	return value
+
+if __name__=="__main__":				    
+	lista=sys.argv[1].split(',')
+	result=average(lista)
+	print result
+	
+	
+	
+	
