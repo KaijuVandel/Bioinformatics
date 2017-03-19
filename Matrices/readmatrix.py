d646fe1c5e2a651f925f6a378903816f15718d46
Andrea Vandelli <andrea.vandelli94@gmail.com>
2017-03-19 16:18:07 +0100

    readmatrix.py added

--- Matrices/readmatrix.py
+++ Matrices/readmatrix.py
@@ -1,12 +1,19 @@
+import sys
+
 def readmatrix(filename):
+	'''A function to read a matrix from a file'''
         f=open(filename)
         header=f.readline().rstrip().split()
         M={}
         for line in f:
-                line.rstrip().split()
+                line=line.rstrip().split()
                 label=line[0]
-                print label
                 for i in range(1,len(line)):
                         M[(label,header[i-1])]=float(line[i])
         return M
         f.close()
+
+if __name__=="__main__":
+	filepath=sys.argv[1]
+	matrix=readmatrix(filepath)
+	print matrix
